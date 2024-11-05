5a78abd89b59151047ae9c9aee2188f727659bd5
Blazej Mcszk <dzikun@gmail.com>
2024-11-05 13:20:52 +0100

    fix: fix

--- dist/index.js
+++ dist/index.js
@@ -32024,7 +32024,7 @@ function getLastSuccessfulRun() {
     const payload = github.context.payload;
     const octokit = github.getOctokit(core.getInput('github-token'));
     let result = null;
-    octokit.rest.listWorkflowRuns({
+    octokit.rest.actions.listWorkflowRuns({
         // owner: payload.repository_owner,
         // repo: payload.repository.split('/')[1],
         owner: github.context.repo.owner,
